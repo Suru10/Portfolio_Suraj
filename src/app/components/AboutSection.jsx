f9da336648f8e3134639f9055120c156355c7378
Suraj Patel <70411604+Suru10@users.noreply.github.com>
2023-12-25 20:56:22 -0500

    Initial_items without node_module

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,100 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Deep Learning</li>
+        <li>Machine Learning</li>
+        <li>Pytorch</li>
+        <li>LLM</li>
+        <li>Django</li>
+        <li>Node.js</li>
+        <li>AWS</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Masters: New Jersey Institute of Technology, New Jersey</li>
+        <li>Bachelors: Rutgers New Brunswick, New Jersey</li>
+        <li>Associates: Middlesex County College, New Jersey</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Lead Data Science Intern, AI CAMP</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/ai-brain.jpg" width={1500} height={1500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I am a machine learning engineer dedicated to developing advanced algorithms and models
+           that turn complex data into actionable insights, driving innovation and efficiency in various
+            industries. I have experience working with LLM, Pytorch, Deep Learning, Machine Learning, AWS, 
+            Node.js, React, and Django. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing data driven applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
