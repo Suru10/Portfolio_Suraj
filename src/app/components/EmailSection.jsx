38217fff72b509f2ad164f601b11fe05973b3439
Suraj Patel <70411604+Suru10@users.noreply.github.com>
2023-12-26 15:00:59 -0500

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -40,22 +40,16 @@ const EmailSection = () => {
   };
 
   return (
-    <section
-      id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
-    >
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-      <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
-        </h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
-          try my best to get back to you!
-        </p>
-        <div className="socials flex flex-row gap-2">
+    <section id="contact" className="flex flex-col items-center justify-center w-full h-screen">
+  <div className="text-center">
+    <h5 className="text-xl font-bold text-white my-2">
+      Let's Connect
+    </h5>
+    <p className="text-[#ADB7BE] mb-4 max-w-md">
+      I'm currently looking for new opportunities, my inbox is always open. Whether you have a question, or just want to say hi. 
+      I will do my best to reach out to you!
+    </p>
+    <div className="socials flex flex-row gap-2 justify-center">
           <Link href="https://github.com/Suru10">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
@@ -64,7 +58,7 @@ const EmailSection = () => {
           </Link>
         </div>
       </div>
-      <div>
+      {/* <div>
         {emailSubmitted ? (
           <p className="text-green-500 text-sm mt-2">
             Email sent successfully!
@@ -125,7 +119,7 @@ const EmailSection = () => {
             </button>
           </form>
         )}
-      </div>
+      </div> */}
     </section>
   );
 };
